6060ee9c9d7dcac180d150b0675e239d873923aa
azizy5203 <azizy5203@gmail.com>
2024-10-07 01:04:34 +0300

    chore : shadcn & vercel setup

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+function Home() {
+  return (
+    <div className="p-4 text-7xl">
+      <h1 className="">Home</h1>
+      <Button className="text-foreground">Click me</Button>
+    </div>
+  );
+}
+
+export default Home;
