032dfc8f93191594a1c4cd3ce010a8824d83febf
Youssef Abdulaziz <azizy5203@gmail.com>
2024-10-07 09:39:54 +0300

    feat : home layout

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,8 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Logo from "@/assets/img/logo";
 function HomeLayout() {
   return (
     <div className=" flex flex-col gap-4 p-4 h-screen">
-      <div className="">Home Layout</div>
+      <div className="">
+        <h4>Animator</h4>
+        <img
+          src={Logo}
+          alt=""
+        />
+      </div>
 
       <div className="">
         <Outlet />
